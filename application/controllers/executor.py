6c03eb75cb22feab68faa92b26817b304e4e9776
Mikko Takala <mikkotakala@hotmail.com>
2019-09-07 16:25:14 +0300

    CRUD toiminnallisuus 'huoltomiehelle'

--- application/controllers/executor.py
+++ application/controllers/executor.py
@@ -14,12 +14,18 @@ def executors_new():
     return render_template("executors/new.html")
 
 @app.route("/executors/<id>/edit")
-def executors_edit():
-    return render_template("executors/edit.html")
+def executors_edit(id):
+    item = Executor.query.get(id)
+    db.session().commit()
+
+    return render_template("executors/edit.html", executor = item)
 
 @app.route("/executor/<id>")
-def executors_get_one():
-    return render_template("executors/executor.html")
+def executors_get_one(id):
+    new = Executor.query.get(id)
+    db.session().commit()
+
+    return render_template("executors/executor.html", executor = new)
 
 @app.route("/executor/", methods=["POST"])
 def executors_add_one():
@@ -30,10 +36,20 @@ def executors_add_one():
 
     return render_template("executors/executor.html", executor = new)
 
-@app.route("/executor/<id>", methods=["PUT"])
-def executors_modify_one():
-    return render_template("executors/executor.html")
+@app.route("/executor/<id>", methods=["POST"])
+def executors_modify_one(id):
+    item = Executor.query.get(id)
+    data = request.form
+
+    if data.get('name'):
+        item.name = data.get('name')
+    if data.get('title'):
+        item.title = data.get('title')
+    
+    db.session().commit()
+
+    return render_template("executors/executor.html", executor = item)
 
 @app.route("/executor/<id>", methods=["DELETE"])
-def executors_delete_one():
+def executors_delete_one(id):
     return render_template("executors/executors.html")
